4542c758a9dcd731f39e501eccb4b6d52515c21f
Carlos Al <crsaele@gmail.com>
2024-05-18 14:55:16 -0600

    added index.html file and another javascript file to test

---
+++ sum-of-numbers.js
@@ -0,0 +1,9 @@
+/* Create a function that takes two numbers as arguments and returns their sum. */
+
+let sumOfTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+console.log(sumOfTwoNumbers(3, 2));
+console.log(sumOfTwoNumbers(-3, -6));
+console.log(sumOfTwoNumbers(7, 3));
